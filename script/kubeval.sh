3e26c4d9f67ea47479257b8a0d8e144aa65ee314
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-11-01 16:09:45 +0200

    Add some logging

--- script/kubeval.sh
+++ script/kubeval.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 set -euo pipefail
 
+function log() {
+  echo -e "\033[1;33m$1\033[0m"
+}
+
 CT_SOURCE="https://github.com/helm/chart-testing/releases/download/v${CT_VERSION}/chart-testing_${CT_VERSION}_linux_amd64.tar.gz"
 KUBEVAL_SOURCE="https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz"
-SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 # install ct
-echo "Download ct"
+log "Download ct"
 curl --silent --show-error --fail --location --output /tmp/ct.tar.gz "${CT_SOURCE}" && tar -xf /tmp/ct.tar.gz ct
 
 # install kubeval
-echo "Download kubeval"
+log "Download kubeval"
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz  "${KUBEVAL_SOURCE}" && tar -xf /tmp/kubeval.tar.gz kubeval
 
 # validate charts
 for CHART_DIR in $(./ct list-changed); do
-  helm template "${CHART_DIR}" | ./kubeval --strict --kubernetes-version "${KUBERNETES_VERSION}" --schema-location "${SCHEMA_LOCATION}"
+  log "Validating $CHART_DIR"
+  helm template "${CHART_DIR}" | ./kubeval --strict --kubernetes-version "${KUBERNETES_VERSION}"
 done
