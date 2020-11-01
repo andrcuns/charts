5cce4daf940505aa4120c974f87fdc0b6d919c33
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-11-01 17:55:05 +0200

    Cache kubeval and ct

--- script/kubeval.sh
+++ script/kubeval.sh
@@ -5,17 +5,24 @@ function log() {
   echo -e "\033[1;33m$1\033[0m"
 }
 
+KUBEVAL_DIR="tools"
+KUBEVAL="$KUBEVAL_DIR/kubeval"
+CT="$KUBEVAL_DIR/ct"
 CT_SOURCE="https://github.com/helm/chart-testing/releases/download/v${CT_VERSION}/chart-testing_${CT_VERSION}_linux_amd64.tar.gz"
 KUBEVAL_SOURCE="https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz"
 
-# install ct
-curl --silent --show-error --fail --location --output /tmp/ct.tar.gz "${CT_SOURCE}" && tar -xf /tmp/ct.tar.gz ct
+if [ ! -d "$KUBEVAL_DIR" ]; then
+  mkdir -p "$KUBEVAL_DIR"
 
-# install kubeval
-curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz  "${KUBEVAL_SOURCE}" && tar -xf /tmp/kubeval.tar.gz kubeval
+  # install ct
+  curl --silent --show-error --fail --location --output /tmp/ct.tar.gz "${CT_SOURCE}" && tar -xf /tmp/ct.tar.gz "$CT"
+
+  # install kubeval
+  curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz "${KUBEVAL_SOURCE}" && tar -xf /tmp/kubeval.tar.gz "$KUBEVAL"
+fi
 
 # validate charts
-for CHART_DIR in $(./ct list-changed); do
+for CHART_DIR in $(./$CT list-changed); do
   log "Validating $CHART_DIR"
-  helm template "${CHART_DIR}" | ./kubeval --strict
+  helm template "${CHART_DIR}" | ./$KUBEVAL --strict
 done
