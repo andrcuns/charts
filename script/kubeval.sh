e1892c81eda36e08baa4e143e3f05992db6a1571
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-11-01 15:09:08 +0200

    Use ct to get changed charts

--- script/kubeval.sh
+++ script/kubeval.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-CHART_DIRS="$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | grep '[cC]hart.yaml' | sed -e 's#/[Cc]hart.yaml##g')"
-KUBEVAL_VERSION="0.15.0"
+CT_SOURCE="https://github.com/helm/chart-testing/releases/download/v${CT_VERSION}/chart-testing_${CT_VERSION}_linux_amd64.tar.gz"
+KUBEVAL_SOURCE="https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz"
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
+# install ct
+curl --silend --show-error --fail --location --output /tmp/ct.tar.gz "${CT_SOURCE}" && tar -xf /tmp/ct.tar.gz ct
 # install kubeval
-curl --silent --show-error --fail --location \
-  --output /tmp/kubeval.tar.gz https://github.com/instrumenta/kubeval/releases/download/"${KUBEVAL_VERSION}"/kubeval-linux-amd64.tar.gz
-tar -xf /tmp/kubeval.tar.gz kubeval
+curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz  "${KUBEVAL_SOURCE}" && tar -xf /tmp/kubeval.tar.gz kubeval
 
 # validate charts
-for CHART_DIR in ${CHART_DIRS}; do
+for CHART_DIR in $(ct list-changed); do
   helm template "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION}" --schema-location "${SCHEMA_LOCATION}"
 done
