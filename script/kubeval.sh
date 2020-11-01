b06dd07032a576fdff0663e3194b1e83a1b24daa
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-11-01 18:30:23 +0200

    Remove manual ct installation

--- script/kubeval.sh
+++ script/kubeval.sh
@@ -5,25 +5,16 @@ function log() {
   echo -e "\033[1;33m$1\033[0m"
 }
 
-KUBEVAL_DIR="tools"
-KUBEVAL="$KUBEVAL_DIR/kubeval"
-CT="$KUBEVAL_DIR/ct"
-CT_SOURCE="https://github.com/helm/chart-testing/releases/download/v${CT_VERSION}/chart-testing_${CT_VERSION}_linux_amd64.tar.gz"
 KUBEVAL_SOURCE="https://github.com/instrumenta/kubeval/releases/download/${KUBEVAL_VERSION}/kubeval-linux-amd64.tar.gz"
 
-if [ ! -d "$KUBEVAL_DIR" ]; then
-  mkdir -p "$KUBEVAL_DIR"
-
-  log "Downloading chart-testing v${CT_VERSION}"
-  curl --silent --show-error --fail --location --output /tmp/ct.tar.gz "${CT_SOURCE}" && tar -xf /tmp/ct.tar.gz ct
-
+if [ ! -f "kubeval" ]; then
   log "Downloading kubeval v${KUBEVAL_VERSION}"
   curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz "${KUBEVAL_SOURCE}" && tar -xf /tmp/kubeval.tar.gz kubeval
-
-  mv ct kubeval $KUBEVAL_DIR/
 fi
 
-CHARTS=$(./$CT list-changed)
+git fetch origin master >/dev/null 2>&1
+
+CHARTS=$(git diff --find-renames --name-only "$(git rev-parse --abbrev-ref HEAD)" remotes/origin/master -- charts | cut -d/ -f 1-2 | sort | uniq)
 
 if [ -z "$CHARTS" ]; then
   log "No chart changes detected."
@@ -33,5 +24,5 @@ fi
 # validate charts
 for CHART_DIR in "$CHARTS"; do
   log "Validating $CHART_DIR"
-  helm template "${CHART_DIR}" | ./$KUBEVAL --strict
+  helm template "${CHART_DIR}" | ./kubeval --strict
 done
