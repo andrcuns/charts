b8e4b002abc44ed4c40060fee10762d4ba15705a
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-11-01 15:12:01 +0200

    Fix kubeval

--- script/kubeval.sh
+++ script/kubeval.sh
@@ -6,11 +6,14 @@ KUBEVAL_SOURCE="https://github.com/instrumenta/kubeval/releases/download/${KUBEV
 SCHEMA_LOCATION="https://raw.githubusercontent.com/instrumenta/kubernetes-json-schema/master/"
 
 # install ct
-curl --silend --show-error --fail --location --output /tmp/ct.tar.gz "${CT_SOURCE}" && tar -xf /tmp/ct.tar.gz ct
+echo "Download ct"
+curl --silent --show-error --fail --location --output /tmp/ct.tar.gz "${CT_SOURCE}" && tar -xf /tmp/ct.tar.gz ct
+
 # install kubeval
+echo "Download kubeval"
 curl --silent --show-error --fail --location --output /tmp/kubeval.tar.gz  "${KUBEVAL_SOURCE}" && tar -xf /tmp/kubeval.tar.gz kubeval
 
 # validate charts
-for CHART_DIR in $(ct list-changed); do
+for CHART_DIR in $(./ct list-changed); do
   helm template "${CHART_DIR}" | ./kubeval --strict --ignore-missing-schemas --kubernetes-version "${KUBERNETES_VERSION}" --schema-location "${SCHEMA_LOCATION}"
 done
