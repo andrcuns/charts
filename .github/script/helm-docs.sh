928697d2eeebe60eccede67d4d04ec7ae92e5cee
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-11-01 23:08:46 +0200

    Skip whole job when no changes

--- .github/script/helm-docs.sh
+++ .github/script/helm-docs.sh
@@ -4,13 +4,6 @@ set -euo pipefail
 
 source "$(dirname "$0")/utils.sh"
 
-charts=($(changed-charts))
-
-if [ ${#charts[@]} -eq 0 ]; then
-  log "No chart changes detected."
-  exit
-fi
-
 install-helmdocs
 
 helm-docs -c charts
--- .github/script/kubeval.sh
+++ .github/script/kubeval.sh
@@ -6,11 +6,6 @@ source "$(dirname "$0")/utils.sh"
 
 charts=($(changed-charts))
 
-if [ ${#charts[@]} -eq 0 ]; then
-  log "No chart changes detected."
-  exit
-fi
-
 install-kubeval
 
 for chart_dir in "${charts[@]}"; do
