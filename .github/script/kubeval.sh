f9575c40ecc45f66e128b9917484bf11d45551ae
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2020-11-05 21:06:03 +0200

    Support multiple ci-values for kubeval

--- .github/script/kubeval.sh
+++ .github/script/kubeval.sh
@@ -9,10 +9,18 @@ charts=($(changed-charts))
 install-kubeval
 
 for chart_dir in "${charts[@]}"; do
-  values_yaml="$chart_dir/ci/ci-values.yaml"
-  values_opt=$([ -f "$values_yaml" ] && echo "-f $values_yaml" || echo "")
+  for yaml in $chart_dir/ci/ci-*.yaml; do
+    values_opt=$([ -f "$yaml" ] && echo "-f $yaml" || echo "")
 
-  log "Validating $chart_dir"
-  helm dependency update $chart_dir
-  helm template $chart_dir $values_opt | kubeval --strict
+    message="Validating $chart_dir with $yaml"
+    length=$(echo "$message" | awk '{print length}')
+    delimiter=$(head -c $length </dev/zero | tr '\0' '=')
+
+    log "$delimiter"
+    log "$message"
+    log "$delimiter"
+
+    helm dependency update $chart_dir
+    helm template $chart_dir $values_opt | kubeval --strict
+  done
 done
