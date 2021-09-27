25b981b5077b4999712108b2a87a66dac3d8ac18
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2021-09-27 23:41:47 +0300

    chore: improve release script logging

--- script/release.sh
+++ script/release.sh
@@ -15,9 +15,11 @@ version_segment=${2:-patch}
 log "bump chart version"
 helm local-chart-version bump -c charts/$chart -s $version_segment
 
+log
 log "update docs"
 helm-docs
 
-log "commit changes"
+log
+log "create release commit"
 version=$(helm local-chart-version get -c charts/$chart)
 git commit -a -m "$chart: Update chart to version $version"
