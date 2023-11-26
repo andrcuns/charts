8ffdd6122fddfed9c982b980611fc5937bb68a95
Andrejs Cunskis <andrejs.cunskis@gmail.com>
2023-11-26 18:20:38 +0200

    Remove empty log messages

--- .github/script/bump-version.sh
+++ .github/script/bump-version.sh
@@ -14,11 +14,9 @@ git config user.email github-actions@github.com
 log "bump chart version"
 helm local-chart-version bump -c charts/$chart -s $version_segment
 
-log
 log "update docs"
 helm-docs -c charts/$chart
 
-log
 log "create release commit"
 version=$(helm local-chart-version get -c charts/$chart)
 git commit -a -m "$chart: Update chart to version $version"
